59b7b4e523a139a0eebab6206318e090fb3443cf
webala <webala1001@gmail.com>
2021-12-29 14:52:21 +0300

    store styling complete

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    #own
+    'store',
+    #third party
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
